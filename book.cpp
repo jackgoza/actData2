17b172ba7ae3e458fba9d16711518d6341ea79ad
Jack G <jgkp9@umkc.edu>
2016-07-04 10:36:22 -0400

    Initial file creation
    Barebones file creation focusing on making sure we portion out these
    functions correctly

---
+++ book.cpp
@@ -0,0 +1,9 @@
+//
+//  book.cpp
+//  data2
+//
+//  Created by John Goza on 7/4/16.
+//  Copyright © 2016 JohnGoza. All rights reserved.
+//
+
+#include "book.hpp"
---
+++ main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  data2
+//
+//  Created by John Goza on 7/4/16.
+//  Copyright © 2016 JohnGoza. All rights reserved.
+//
+
+#include <iostream>
+#include <queue>
+#include <list>
+#include "book.hpp"
+
+
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
